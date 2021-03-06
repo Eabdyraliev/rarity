f8d3e390316ddf00fa464026bf0ffd4667cb018f
Eabdyraliev <Eabdyraliev@gmail.com>
2021-03-06 12:43:03 +0600

    create books.html

--- app.py
+++ app.py
@@ -20,21 +20,23 @@ def books():
     excel = load_workbook('tales.xlsx')
     page = excel['Лист1']
 
-    tales = [tale.value for tale in page['A']][1:]
+    object_list = [[tale.value, tale.offset(column=1).value] for tale in page ['A'][:1]]
+    return render_template('books.html', object_list=object_list)
+    # tales = [tale.value for tale in page['A']][1:]
 
 
-    authors = [author.value for author in page['B']][1:]
+    # authors = [author.value for author in page['B']][1:]
 
-    html = '''
-           <a href='/books'>Книги</a>
-           <a href='/authors/'>авторы </a>
-           <h1> Тут будет список книг <h1>
-           '''
+    # html = '''
+    #        <a href='/books'>Книги</a>
+    #        <a href='/authors/'>авторы </a>
+    #        <h1> Тут будет список книг <h1>
+    #        '''
 
-    for i in range(len(tales)):
-        html += f'<h2>{tales[i]} - {authors[i]}<h2>' 
+    # for i in range(len(tales)):
+    #     html += f'<h2>{tales[i]} - {authors[i]}<h2>' 
     
-    return html
+    # return html
 
 
 
@@ -52,7 +54,7 @@ def add:()
     
     page = excel ['Лист1']
     l_book = len(page['A']) + 1
-    excel = [f'A'{'l_book'}] = f['author']
-    excel = [f'B'{'l_book'}] = f['book']
-    l_author = 
+    excel = [f'A'{'l_book'}] = f['book']
+    excel = [f'B'{'l_book'}] = f['author']
+    excel.save('tales.xlsx')
     return 'форма получена'
