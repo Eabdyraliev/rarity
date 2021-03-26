1e92480231057d0fbc7918209036178ff1f76012
Eabdyraliev <Eabdyraliev@gmail.com>
2021-03-26 21:15:47 +0600

    finish rarity

--- app.py
+++ app.py
@@ -1,27 +1,43 @@
 from flask import Flask, render_template, request
 from openpyxl import load_workbook
 from sqlalchemy.orm import sessionmaker
-from database import engine
+from sqlalchemy.orm import scoped_session
+from database import engine, Book
 
 app = Flask(__name__)
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route('/')
 def home_page():
     return render_template ('index.html')
 
+@app.route('/form/')
+def form():
+    return render_template('form.html')
+
 @app.route('/books/')
 def books():
-    # session = sessionmaker(engine)()
-    # books = session.query('Book')
-    # print(books)
-    # session.commit()
-    # return render_template('books_table.html', objects_list=books)
+    if 'key_word' in request.args:
+        key_word = request.args.get("key_word")
+        session = sessionmaker(engine)()
+        books = session.execute(f"""
+            SELECT * FROM "Book"
+            WHERE name LIKE '%{key_word}%'
+                OR author LIKE '%{key_word}%'
+            ;
+        """)
+        session.commit()
+    else:
+        with engine.connect() as con:
+            books = con.execute("""SELECT * FROM "Book";""")
 
-    excel = load_workbook('tales.xlsx')
-    page = excel['Лист1']
+    return render_template("books_table.html", object_list=books)
 
-    object_list = [[tale.value, tale.offset(column=1).value] for tale in page ['A'][:1]]
-    return render_template('books.html', object_list=object_list)
+    # excel = load_workbook('tales.xlsx')
+    # page = excel['Лист1']
+
+    # object_list = [[tale.value, tale.offset(column=1).value] for tale in page ['A'][1:]]
+    # return render_template('books.html', object_list=object_list)
     # tales = [tale.value for tale in page['A']][1:]
 
 
@@ -40,31 +56,62 @@ def books():
 
 
 
-@app.route('/authors/')
-def authors():
-    excel = load_workbook('tales.xlsx')
-    page = excel['Лист1']
-    authors = {author.value for author in page['B'][1:]}
-    return render_template("authors.html" authors=list(authors))
+@app.route("/authors/")
+def db_authors():
+    with engine.connect() as con:
+        authors = con.execute('SELECT DISTINCT author FROM "Book";')
 
-@app.route('/add/', methods = ['POST'])
-def add:()
-    f = request.forms
-    excel = load_workbook ('tales.xlsx')
+    return render_template(
+        "authors_table.html", authors=authors
+    )
+
+@app.route("/db/add/", methods=["POST"])
+def db_add():
+    f = request.form
+    book = f["book"]
+    author = f["author"]
+    url = f["url"]    
     
-    page = excel ['Лист1']
-    l_book = len(page['A']) + 1
-    excel = [f'A'{'l_book'}] = f['book']
-    excel = [f'B'{'l_book'}] = f['author']
-    excel.save('tales.xlsx')
-    return 'форма получена'
+    ids = db.execute('SELECT id FROM "Book" ORDER BY id DESC;')
+    max_id = ids.first().id
+    c_id = max_id + 1
 
-@app.route('/books/<num>/')
-def book(num):
+    db.execute(f'''
+        INSERT INTO "Book" (id, name, author, image)
+        VALUES ({c_id}, '{book}', '{author}', '{url}');''')
+    
+    db.commit()
 
-    excel = load_workbook('tales.xlsx')
-    page = excel['Лист1']
+    return "форма получена"
 
-    object_list = [[tale.value, tale.offset(column=1).value, tale.offset(column=2).value] for tale in page ['A'][:1]]
-    obj = object_list[int(num)]
-    return render_template('book.html', objt=obj)
+
+@app.route("/db/book/<id>/")
+def db_book(id):
+    obj = db.execute(f'SELECT * FROM "Book" WHERE id = {id};').first()
+    return render_template("database_book.html", obj=obj)
+    
+
+@app.route("/<int:id>", methods=['GET', 'POST'])
+def db_book_update(id):
+    message = ''
+    name = request.form.get('tale')
+    author = request.form.get('author')
+    image =request.form.get('image')
+    if request.method == 'POST':
+        db.execute(f'''
+        UPDATE "Book" 
+        SET 
+            name='{name}',
+            author='{author}',
+            image='{image}'
+        WHERE id={id};
+        ''')
+        db.commit()
+        message = 'Измения сохранены и находятся на стадии обработки'
+
+    book_obj = db.execute(f'SELECT * FROM "Book" WHERE id = {id};').first()
+    return render_template("db_book_update.html", book_obj=book_obj, message=message)
+
+@app.route('/about_us/')
+def about_us():
+    return render_template('about_us.html')
--- database.py
+++ database.py
@@ -1,16 +1,16 @@
 from sqlalchemy import create_engine, Column, Integer, VARCHAR, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://postgres:admin@localhost:5432/rarity_db')
+engine = create_engine("postgresql://postgres:admin@localhost:5432/rarity_db")
 Base = declarative_base()
 
 
 class Book(Base):
-    __tablename__= 'Book'
+    __tablename__ = "Book"
 
-    id = Column(Integer, Sequence('some_id,seq'), primary_key = True)
-    name = Column,m                                                                                                                                                                                                                                                                                         cczz(VARCHAR(255), nullable = False)
-    author = Column(VARCHAR(255), nullable = False)
+    id = Column(Integer, Sequence('some_id_seq'), primary_key=True)
+    name = Column(VARCHAR(255), nullable=False)
+    author = Column(VARCHAR(255), nullable=False)
     image = Column(VARCHAR(255))
 
 
